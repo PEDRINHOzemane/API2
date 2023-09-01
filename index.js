076441894e5cf7a168daaa20cfab11c2aea0d098
PEDRINHOzemane <132668867+PEDRINHOzemane@users.noreply.github.com>
2023-09-01 11:50:38 +0000

    2

--- index.js
+++ index.js
@@ -12,3 +12,33 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
 app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
 });
+
+//index.js
+import dotenv from "dotenv";
+import { selectUsuarios } from "./bd.js";
+
+dotenv.config();
+
+
+/*
+
+app.get("/", (req, res) => {
+  console.log("Rota / solicitada");
+  // Cria a rota da raiz do projeto
+  res.json({
+    nome: "Arthur Porto", // Substitua pelo seu nome
+  });
+});
+
+*/
+
+app.get("/usuarios", async (req, res) => {
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+
+  console.log("Rota GET/usuarios solicitada");
+});
